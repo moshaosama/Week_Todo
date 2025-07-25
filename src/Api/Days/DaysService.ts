2ebf6d6f051c8201c0439e92c82dfbeb14f2e198
moshaosama <mohamedOSFekry@gmail.com>
2025-07-26 01:47:02 +0300

    feat(api): add new API module

---
+++ src/Api/Days/DaysService.ts
@@ -0,0 +1,15 @@
+import { ParentService } from "../parentService";
+import axios from "axios";
+class DayService extends ParentService {
+  CreateData() {}
+  async getData(user_id?: number, thunkApi?: any) {
+    try {
+      const response = await axios.get(`http://localhost:3000/days/${user_id}`);
+      return response.data;
+    } catch (err) {
+      return thunkApi.rejectWithValue(err);
+    }
+  }
+}
+
+export const dayService = new DayService();
