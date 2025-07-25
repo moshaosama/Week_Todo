5fd4052948e82e2ca8f17b9d29df9d71fa0e7375
moshaosama <mohamedOSFekry@gmail.com>
2025-07-26 01:00:21 +0300

    refactor(layout): update Sidebar layout component

--- src/Layouts/Sidebar.tsx
+++ src/Layouts/Sidebar.tsx
@@ -2,24 +2,44 @@ import { CgMoreAlt } from "react-icons/cg";
 import { DefaultIcons } from "../Constants/Icons";
 import { IoInformationCircleOutline } from "react-icons/io5";
 import { CiSettings } from "react-icons/ci";
+import { Outlet } from "react-router-dom";
 
 const Sidebar_Navbar = () => {
   return (
     <>
-      <div className="p-4 flex flex-col justify-between h-[100vh] items-center w-20">
-        <div className="flex flex-col justify-center items-center">
-          <img src="/logo-color.svg" alt="logoBrand.png" className="w-14" />
-          <div className="flex flex-col gap-7 mt-4">
-            {DefaultIcons?.map(({ icon }) => {
-              return <i>{icon}</i>;
-            })}
+      <div className="flex gap-3 ">
+        <div className="p-4 flex flex-col justify-between h-[100vh] items-center w-20">
+          <div className="flex flex-col justify-center items-center">
+            <img src="/logo-color.svg" alt="logoBrand.png" className="w-14" />
+            <div className="flex flex-col gap-7 mt-4">
+              {DefaultIcons?.map(({ icon }) => {
+                return (
+                  <i className="hover:bg-gray-300 cursor-pointer transition-all duration-300 p-2 rounded-full">
+                    {icon}
+                  </i>
+                );
+              })}
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-7 justify-center items-center">
+            <CgMoreAlt
+              size={50}
+              className="hover:bg-gray-300 cursor-pointer transition-all duration-300 p-2 rounded-full"
+            />
+            <IoInformationCircleOutline
+              size={50}
+              className="hover:bg-gray-300 cursor-pointer transition-all duration-300 p-2 rounded-full"
+            />
+            <CiSettings
+              size={50}
+              className="hover:bg-gray-300 cursor-pointer transition-all duration-300 p-2 rounded-full"
+            />
           </div>
         </div>
 
-        <div className="flex flex-col gap-7 justify-center items-center">
-          <CgMoreAlt size={30} />
-          <IoInformationCircleOutline size={30} />
-          <CiSettings size={30} />
+        <div className="p-7">
+          <Outlet />
         </div>
       </div>
     </>
