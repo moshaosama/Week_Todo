c3861d67956788ea01bd10bf4c64f38ea78159a1
moshaosama <mohamedOSFekry@gmail.com>
2025-07-25 16:53:30 +0300

    feat(layouts): add new layouts components

---
+++ src/Layouts/Sidebar.tsx
@@ -0,0 +1,29 @@
+import { CgMoreAlt } from "react-icons/cg";
+import { DefaultIcons } from "../Constants/Icons";
+import { IoInformationCircleOutline } from "react-icons/io5";
+import { CiSettings } from "react-icons/ci";
+
+const Sidebar_Navbar = () => {
+  return (
+    <>
+      <div className="p-4 flex flex-col justify-between h-[100vh] items-center w-20">
+        <div className="flex flex-col justify-center items-center">
+          <img src="/logo-color.svg" alt="logoBrand.png" className="w-14" />
+          <div className="flex flex-col gap-7 mt-4">
+            {DefaultIcons?.map(({ icon }) => {
+              return <i>{icon}</i>;
+            })}
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-7 justify-center items-center">
+          <CgMoreAlt size={30} />
+          <IoInformationCircleOutline size={30} />
+          <CiSettings size={30} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar_Navbar;
