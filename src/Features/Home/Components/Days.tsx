b8ac810bc70e95e594db270ce8566a090031249e
moshaosama <mohamedOSFekry@gmail.com>
2025-07-26 23:53:59 +0300

    feat(home): improve Days component UI and logic

--- src/Features/Home/Components/Days.tsx
+++ src/Features/Home/Components/Days.tsx
@@ -1,22 +1,27 @@
-import Inputs from "../../../Components/inputs";
+import { CgMoreVerticalAlt } from "react-icons/cg";
 import useGetDays from "../hooks/useGetDays";
+import Tasks from "./Tasks";
 
 const Days = () => {
   const { Days } = useGetDays();
-
   return (
     <>
-      <div className="flex justify-between items-center">
-        {Days?.data?.map((el: any, index: number) => (
-          <div key={index} className="flex flex-col">
-            <div className="flex flex-col items-center gap-2">
-              <h1 className="text-3xl font-semibold">{el || el.dayName}</h1>
-              <p className="text-sm text-[grey]">July 26, 2025</p>
+      <div className="flex justify-between overflow-y-scroll h-[50vh]">
+        {Days?.data?.data?.map((el: any, index: number) => (
+          <>
+            <div key={index} className="flex flex-col">
+              <div className="flex w-full gap-10 justify-between items-center group">
+                <div className="flex flex-col items-center gap-2">
+                  <h1 className="text-3xl font-semibold">{el || el.dayName}</h1>
+                  <p className="text-sm text-[grey]">July 26, 2025</p>
+                </div>
+                <div className="hidden group-hover:block cursor-pointer">
+                  <CgMoreVerticalAlt size={25} />
+                </div>
+              </div>
+              <Tasks day_id={Number(Days?.data?.ids[index])} />
             </div>
-            <div className="flex flex-col">
-              <Inputs />
-            </div>
-          </div>
+          </>
         ))}
       </div>
     </>
