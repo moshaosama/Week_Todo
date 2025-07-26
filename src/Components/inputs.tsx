8c7b21b73e99b21774d62e136e2e85f9f4eedd02
moshaosama <mohamedOSFekry@gmail.com>
2025-07-26 20:35:22 +0300

    feat(components): enhance Inputs component UI and logic

--- src/Components/inputs.tsx
+++ src/Components/inputs.tsx
@@ -1,11 +1,10 @@
 const Inputs = () => {
   const arr = Array(14).fill(0);
-  console.log(arr);
 
   return (
     <>
-      {arr.map((_) => (
-        <form className="flex flex-col">
+      {arr.map((_, i: number) => (
+        <form className="flex flex-col" key={i}>
           <input
             type="text"
             className="w-48 focus:outline-0 border-b-2 border-b-gray-600"
