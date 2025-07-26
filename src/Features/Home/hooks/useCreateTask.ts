59257b41fc59a425896235037c109746d436846b
moshaosama <mohamedOSFekry@gmail.com>
2025-07-26 23:54:21 +0300

    feat(home): add useCreateTask hook for task creation

---
+++ src/Features/Home/hooks/useCreateTask.ts
@@ -0,0 +1,25 @@
+import { useMutation } from "@tanstack/react-query";
+import { taskService } from "../../../Api/Tasks/taskService";
+import useGetUser from "../../../Hooks/useGetUser";
+import { useForm } from "react-hook-form";
+
+const useCreateTask = () => {
+  const { user } = useGetUser();
+  const { register, handleSubmit } = useForm();
+  const { mutate } = useMutation({
+    mutationKey: ["createTask", user?.data?.[0]?.id],
+    mutationFn: taskService.CreateData,
+  });
+
+  const handleCreateTask = (data: any, day_id: number) => {
+    mutate({
+      day_id,
+      user_id: user?.data?.[0]?.id,
+      task: data.task,
+    });
+  };
+
+  return { register, handleSubmit, handleCreateTask };
+};
+
+export default useCreateTask;
