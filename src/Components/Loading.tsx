5408f7d5632a49fa10ce7f6e9ef4be3f2270e226
moshaosama <mohamedOSFekry@gmail.com>
2025-07-26 23:54:20 +0300

    feat(components): create Loading component

---
+++ src/Components/Loading.tsx
@@ -0,0 +1,12 @@
+const Loading = () => {
+  return (
+    <>
+      <div className="flex justify-center items-center opa h-[100vh] gap-3">
+        <img src="/logo-color.svg" alt="LogoBrand.png" className="w-24" />
+        <h1 className="text-5xl text-[#183251] font-bold">WeekToDo</h1>
+      </div>
+    </>
+  );
+};
+
+export default Loading;
