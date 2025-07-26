04845c633a23b270a0437b99cc3d991685e6d9fe
moshaosama <mohamedOSFekry@gmail.com>
2025-07-26 23:54:20 +0300

    feat(api): add Tasks API module

---
+++ src/Api/Tasks/taskService.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { ParentService } from "../parentService";
+
+class TaskService extends ParentService {
+  async CreateData(
+    data?: { task: string; day_id: number; user_id: number },
+    thunkApi?: any
+  ) {
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/task/create-task",
+        {
+          task: data?.task,
+          user_id: data?.user_id,
+          day_id: data?.day_id,
+        }
+      );
+      return response.data;
+    } catch (err) {
+      return thunkApi.rejectWithValue(err);
+    }
+  }
+  async getData(data?: { user_id: number; day_id: number }, thunkApi?: any) {
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/task/get-task/${data?.user_id}/${data?.day_id}`
+      );
+      return response.data;
+    } catch (err) {
+      return thunkApi.rejectWithValue(err);
+    }
+  }
+}
+
+export const taskService = new TaskService();
