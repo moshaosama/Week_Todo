06c4176f716d70aa58efdc7772df6130c6b09abd
moshaosama <mohamedOSFekry@gmail.com>
2025-07-26 20:07:20 +0300

    feat(home): enhance Days component UI and logic

--- src/Features/Home/Components/Days.tsx
+++ src/Features/Home/Components/Days.tsx
@@ -1,3 +1,4 @@
+import Inputs from "../../../Components/inputs";
 import useGetDays from "../hooks/useGetDays";
 
 const Days = () => {
@@ -6,10 +7,15 @@ const Days = () => {
   return (
     <>
       <div className="flex justify-between items-center">
-        {Days?.data?.map((el, index: number) => (
-          <div key={index} className="flex flex-col items-center gap-2">
-            <h1 className="text-3xl font-semibold">{el}</h1>
-            <p className="text-sm text-[grey]">July 26, 2025</p>
+        {Days?.data?.map((el: any, index: number) => (
+          <div key={index} className="flex flex-col">
+            <div className="flex flex-col items-center gap-2">
+              <h1 className="text-3xl font-semibold">{el || el.dayName}</h1>
+              <p className="text-sm text-[grey]">July 26, 2025</p>
+            </div>
+            <div className="flex flex-col">
+              <Inputs />
+            </div>
           </div>
         ))}
       </div>
