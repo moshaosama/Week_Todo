2f692870fc5cc6871dd63c019aa347ad486e0dda
moshaosama <mohamedOSFekry@gmail.com>
2025-07-29 15:49:03 +0300

    feat(ui): integrate edit functionality in EditTask component

--- src/Features/Home/Components/EditTask.tsx
+++ src/Features/Home/Components/EditTask.tsx
@@ -2,8 +2,8 @@ import { CgMoreVerticalAlt } from "react-icons/cg";
 import { HiXMark } from "react-icons/hi2";
 import { IoMdAddCircleOutline } from "react-icons/io";
 import useGetTaskByid from "../hooks/useGetTaskByid";
-import { useOpenEditTasksContext } from "../Context/openEditTasks";
 import useEditTask from "../hooks/useEditTask";
+import { useOpenEditTasksContext } from "../Context/openEditTasks";
 
 const EditTask = ({
   day_id,
@@ -17,10 +17,11 @@ const EditTask = ({
   const { taskByid } = useGetTaskByid(day_id, task_id);
   const { handleTriggerEditTasks } = useOpenEditTasksContext();
   const { handleEditTask, handleSubmit, register } = useEditTask();
+
   return (
     <>
-      <div className=" backdrop-blur-[1px] bg-black/5 p-20  absolute top-0 left-0 w-full h-[100vh]">
-        <div className="bg-white h-[85vh] w-[109pc] p-5 rounded-xl  shadow-gray-500">
+      <div className=" backdrop-blur-[3px] bg-black/5 p-20  absolute top-0 left-0 w-full h-[100vh]">
+        <div className="bg-white h-[85vh] w-[109pc] p-5 rounded-xl relative  shadow-gray-500">
           <div className="flex justify-between mx-10 items-center">
             <div>
               <h1 className="text-3xl font-bold">Edit Task</h1>
@@ -40,7 +41,7 @@ const EditTask = ({
           <hr className="h-[2px]  border-none bg-gray-300 mt-4 rounded-full" />
           <div className="m-10">
             <form
-              onMouseLeave={handleSubmit((data) =>
+              onSubmit={handleSubmit((data) =>
                 handleEditTask(data, {
                   day_id: day_id,
                   task_id: task_id,
@@ -60,26 +61,30 @@ const EditTask = ({
               </p>
 
               <textarea
-                defaultValue="Notes"
+                defaultValue={"Notes"}
                 className="text-[gray] text-xl focus:h-52"
               />
+
+              <hr className="h-[2px] w-full border-none bg-gray-300  rounded-full" />
+              <p className="flex items-center gap-5">
+                <label htmlFor="Add">
+                  <IoMdAddCircleOutline size={35} color="gray" />
+                </label>
+                <input
+                  type="text"
+                  id="Add"
+                  defaultValue="Add Subtask"
+                  className="w-full p-2 rounded-lg"
+                />
+              </p>
+
+              <p>
+                <button className="bg-green-500 w-40 text-white font-bold rounded-xl cursor-pointer hover:bg-green-600 transition-all duration-300 py-2 absolute bottom-5 right-5">
+                  Save
+                </button>
+              </p>
             </form>
           </div>
-          <hr className="h-[2px] w-full border-none bg-gray-300  rounded-full" />
-
-          <form action="" className="mt-5 mx-10">
-            <p className="flex items-center gap-5">
-              <label htmlFor="Add">
-                <IoMdAddCircleOutline size={35} color="gray" />
-              </label>
-              <input
-                type="text"
-                id="Add"
-                defaultValue="Add Subtask"
-                className="w-full p-2 rounded-lg"
-              />
-            </p>
-          </form>
         </div>
       </div>
     </>
