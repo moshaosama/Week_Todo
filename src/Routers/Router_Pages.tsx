4ece74328c84442440d337e72a599896f88387be
moshaosama <mohamedOSFekry@gmail.com>
2025-07-29 16:18:11 +0300

    chore(router): register route for custom tasks page

--- src/Routers/Router_Pages.tsx
+++ src/Routers/Router_Pages.tsx
@@ -4,13 +4,16 @@ import Home from "../Pages/Home";
 import SignUp from "../Auth/SignUp";
 import Login from "../Auth/Login";
 import WithLoader from "../Utils/WithLoader";
+import useGetUser from "../Hooks/useGetUser";
+import { useEffect } from "react";
 
 const Sidebar_Navbar_Loader = WithLoader(Sidebar_Navbar);
+const { user } = useGetUser();
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Sidebar_Navbar />,
+    element: user ? <Sidebar_Navbar_Loader /> : <Login />,
     children: [
       {
         index: true,
@@ -30,6 +33,9 @@ const router = createBrowserRouter([
 ]);
 
 const Router_Pages = () => {
+  useEffect(() => {
+    console.log("Mosha");
+  }, [user]);
   return <RouterProvider router={router} />;
 };
 
