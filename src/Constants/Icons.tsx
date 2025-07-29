ea3eaa2f594a40c2bf96967e74a4950891d76b4b
moshaosama <mohamedOSFekry@gmail.com>
2025-07-29 15:49:03 +0300

    chore(constants): update icon mappings and structure

--- src/Constants/Icons.tsx
+++ src/Constants/Icons.tsx
@@ -3,26 +3,38 @@ import { GrPowerReset } from "react-icons/gr";
 import { IoIosAddCircleOutline } from "react-icons/io";
 import { IoHomeOutline } from "react-icons/io5";
 import { SlCalender } from "react-icons/sl";
+import useCreateCustomTask from "../Features/Home/hooks/useCreateCustomTask";
 
 interface dataType {
   icon: ReactElement;
+  onClick: () => void;
 }
 
-export const DefaultIcons: dataType[] = [
-  {
-    icon: <IoHomeOutline size={30} />,
-  },
+const Icons = () => {
+  const { handleCreateCustomTask } = useCreateCustomTask();
+  const DefaultIcons: dataType[] = [
+    {
+      icon: <IoHomeOutline size={30} />,
+      onClick: () => {},
+    },
 
-  {
-    icon: <SlCalender size={30} />,
-  },
+    {
+      icon: <SlCalender size={30} />,
+      onClick: () => {},
+    },
 
-  {
-    icon: <GrPowerReset size={30} />,
-  },
+    {
+      icon: <GrPowerReset size={30} />,
+      onClick: () => {},
+    },
 
-  {
-    icon: <IoIosAddCircleOutline size={30} />,
-  },
+    {
+      icon: <IoIosAddCircleOutline size={30} />,
+      onClick: handleCreateCustomTask,
+    },
+  ];
 
-];
+  return { DefaultIcons };
+};
+
+export default Icons;
