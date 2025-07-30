c113c3ede1bfe259d7c46bccf8a9e68cb8dbd621
moshaosama <mohamedOSFekry@gmail.com>
2025-07-30 16:04:56 +0300

    Update Home.tsx

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,11 +1,14 @@
 import Days from "../Features/Home/Components/Days";
 import OpenEditTasksProvider from "../Features/Home/Context/openEditTasks";
+import OpenSidebarDaysProvider from "../Features/Home/Context/OpenSidebarDays";
 
 const Home = () => {
   return (
     <div className="w-[105pc]">
       <OpenEditTasksProvider>
-        <Days />
+        <OpenSidebarDaysProvider>
+          <Days />
+        </OpenSidebarDaysProvider>
       </OpenEditTasksProvider>
     </div>
   );
